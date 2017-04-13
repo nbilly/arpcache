fc8d7f3ebc8f97419bfd1e26f253cfacd4f3fa11
Nicolas Billy <nbilly@paloaltonetworks.com>
2017-04-13 20:13:45 +0200

    Major update

--- arpcheck.py
+++ arpcheck.py
@@ -6,11 +6,21 @@ import re
 import os
 import string
 import random
+from collections import defaultdict
 
 def id_generator(size=6, chars=string.ascii_lowercase + string.digits):
 	return ''.join(random.choice(chars) for _ in range(size))
 
-def processing_log(filename, debug):
+#dict_tiger:
+#key: MAC address
+#[0]: Interface
+#[1]...[x]: vlan tag
+
+#dict_cache:
+#key: MAC address
+#[0]...[x]: ip address
+
+def processing_log(filename,dict_cache, dict_tiger,debug):
 	
 	i_count_cache = 0
 	i_count_tiger = 0
@@ -31,15 +41,23 @@ def processing_log(filename, debug):
 		result = re_cache.search(record)
 		if result:
 			i_count_cache+=1
+			l_result=result.group(0).split()	
+			dict_cache[l_result[2]].append(l_result[1])			
 			if debug:
 				f_debug_cache.write(result.group(0)+'\n')
 		else:
 			result = re_tiger.search(record)
 			if result:
 				i_count_tiger+=1
+				l_result=result.group(0).split()
+				if not dict_tiger.has_key(l_result[2]):
+					dict_tiger[l_result[2]].append(l_result[8])
+					dict_tiger[l_result[2]].append(l_result[3])
+				else:
+					dict_tiger[l_result[2]].append(l_result[3])
+				
 				if debug:
-					f_debug_tiger.write(result.group(0)+'\n')
-					
+					f_debug_tiger.write(result.group(0)+'\n')				
 	if debug:
 		f_debug_tiger.close()
 		f_debug_cache.close()
@@ -48,19 +66,29 @@ def processing_log(filename, debug):
 	print "number of entries in cache: ",i_count_cache," - tiger: ",i_count_tiger
 	
 #Main
-#output_cache=".output_cache.log"
-#output_tiger=".output_tiger.log"
 
 active_log="active.log"
 passive_log="passive.log"
 
-print "Active Firewall"
+
+dict_cache_active=defaultdict(list)
+dict_tiger_active=defaultdict(list)
+
+dict_cache_passive=defaultdict(list)
+dict_tiger_passive=defaultdict(list)
+
+
+print "Active Firewall processing"
 print
-processing_log(active_log,1)
+processing_log(active_log,dict_cache_active,dict_tiger_active,0)
+print len(dict_cache_active), " Different MAC addresses in Cache"
+print len(dict_tiger_active), " Different MAC addresses in Tiger"
 print
-print "Passive Firewall"
+print "Passive Firewall processsing"
 print
-processing_log(passive_log,1)
+processing_log(passive_log,dict_cache_passive,dict_tiger_passive,0)
+print len(dict_cache_passive), " Different MAC addresses in Cache"
+print len(dict_tiger_passive), " Different MAC addresses in Tiger"
 
 
 	
